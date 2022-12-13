9ea0ec0bb3184bdb7b074cd8006c9bb37782c2eb
Garry Potter <vadimgb1@gmail.com>
2022-12-13 02:57:10 +0000

    hello haryy

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express"
+import bodyParser from "body-parser"
 const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) =>
 {
-    res.send("Hello, world!");
+    res.render("index.ejs");
 })
 
+app.post('/', (req, res) =>
+{
+    const username = req.body.username;
+    res.render('hello.ejs', {username: username});
+})
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listen port: ${port}`))
