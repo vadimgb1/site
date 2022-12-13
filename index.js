596eff0cc4f00739a318995497ee3a55dda85129
Garry Potter <vadimgb1@gmail.com>
2022-12-13 02:40:53 +0000

    static site

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express"
+const app = express();
+app.get('/', (req, res) =>
+{
+    res.send("Hello, world!");
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`listen port: ${port}`))
